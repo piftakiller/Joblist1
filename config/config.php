07fd489bb00eb8a6af94b33924718d10d4ed5138
piftakiller <balintpisti96@gmail.com>
2019-12-12 15:46:06 +0100

    0.45
    00

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+//config
+//config file
+define("DB_HOST","localhost");
+define("DB_USER","faszaa");
+define("DB_PASS","012345");
+define("DB_NAME","joblist");
+define("TITLE","JobLister");
---
+++ templates/inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Joblister</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href=" /css/styles.css">
+  </head>
+  <body>
+
+    <div class="container">
+       <div class="header clearfix">
+         <nav>
+           <ul class="nav nav-pills pull-right">
+             <li role="presentation"><a href="index.php">Home</a></li>
+             <li role="presentation"><a href="#">Create Job</a></li>
+           </ul>
+         </nav>
+         <h3 class="text-muted"><?php $TITLE=TITLE; echo $TITLE;?></h3>
+       </div>
